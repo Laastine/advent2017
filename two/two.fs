7e0fb46c84ac90c50c1a6d472533d5277fe72f49
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-02 16:25:43 +0200

    2A done

---
+++ two/two.fs
@@ -0,0 +1,17 @@
+﻿module two
+
+open System
+
+let inputFileToList(fileName: string): List<string> =
+  System.IO.File.ReadLines(fileName) |> Seq.toList
+
+let calc =
+  inputFileToList "./input.txt"
+    |> List.map (fun x -> x.Split('\t') |> Seq.map System.Int32.Parse |> Seq.toList)
+    |> List.map (fun x -> (x |> List.max, x |> List.min))
+    |> List.sumBy (fun x -> fst x - snd x)
+
+[<EntryPoint>]
+let main argv =
+  printfn "%A" calc
+  0
