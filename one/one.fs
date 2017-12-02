9e1b5e209a58de4193f204cb6876ada4946e14df
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-02 09:21:39 +0200

    WIP

--- one/one.fs
+++ one/one.fs
@@ -1,4 +1,6 @@
-﻿open System
+﻿module One
+
+open System
 
 let explode (s: string): List<int> = [for c in s -> c |> string |> int]
 
@@ -6,28 +8,57 @@ let inputFileToList(fileName: string): List<string> =
   System.IO.File.ReadLines(fileName)
   |> Seq.toList
 
-let calculateValue(inputList: List<int>): int =
+
+let calcIndex(idx: int, len: int): int =
+  int (System.Math.Round((((float idx) / (float len)) % 1.0) * (float len)))
+
+let calculateValueA(inputList: List<int>): int =
   let first = inputList.Head
   let rec recur(list: List<int>, acc: int) =
     match list with
     | [] -> acc
     | head::tail ->
-                      // printfn "B: %A::%A %A, first: %A" head tail acc first
-                      if list.Length = 1 && first = head then recur(tail, acc+first)
-                      elif list.Length > 1 && head = tail.Head then recur(tail, acc+head)
-                      else recur(tail, acc)
+        if list.Length = 1 && first = head then recur(tail, acc+first)
+        elif list.Length > 1 && head = tail.Head then recur(tail, acc+head)
+        else recur(tail, acc)
+  recur(inputList, 0)
+
+let calculateValueB(inputList: List<int>): int =
+  let div = inputList.Length / 2
+  let rec recur(list: List<int>, acc: int) =
+    match list with
+    | [] -> acc
+    | head::tail ->
+          let index = inputList.Length - tail.Length - 1
+          let idx =
+            if tail.Length = 0 then
+              div - 1
+            else calcIndex(index+div, inputList.Length)
+          printfn "IDX=%A, idx+%A=%A | head:%A = val:%A => %A" index div idx head inputList.[idx] acc
+          if head = inputList.[idx] then
+            // printfn "A: %A = %A" head inputList.[idx]
+            recur(tail, acc+head)
+          else recur(tail, acc)
   recur(inputList, 0)
 
 let calc: int =
   inputFileToList "./input.txt"
-  // ["1";"1";"2";"2"]
+  // ["1";"2";"3";"1";"2";"3"]
+  // ["1";"2";"1";"2"]
   // ["1";"1";"1";"1"]
   // ["1";"2";"3";"4"]
-  // ["9";"1";"2";"1";"2";"1";"2";"9"]
-  |> List.collect explode
-  |> calculateValue
-
+  // ["1";"2";"1";"3";"1";"4";"1";"5";"1";"1"]
+    |> List.collect explode
+    |> calculateValueB
+//986-967=19
 [<EntryPoint>]
 let main argv =
+
+    // assert (["1";"2";"2";"1"] |> List.collect explode |> calculateValueB = 0)
+    // assert (["1";"2";"2";"4";"2";"5"] |> List.collect explode |> calculateValueB = 4)
+    // assert (["1";"2";"3";"1";"2";"3"] |> List.collect explode |> calculateValueB = 12)
+    // assert (["1";"2";"1";"3";"1";"4";"1";"5"] |> List.collect explode |> calculateValueB = 4)
+
+
     printfn "%A" calc
     0
