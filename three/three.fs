5f6a8d6f4982f1d4d04817ec3d150c5788c4b8a6
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-04 22:04:14 +0200

    3B done

--- three/three.fs
+++ three/three.fs
@@ -1,5 +1,21 @@
 ﻿open System
 
+let nth = 603
+let range = [(nth-1)..(-1)..0]
+
+let calcNeighbours(x: int, y: int, arr: int[,]): int =
+  let getN(a: int, b: int): int =
+    if a >= 0 && a < Array2D.length1 arr && b >= 0 && b < Array2D.length2 arr then arr.[a,b]
+    else 0
+  (getN(x-1,y+1)) + (getN(x,y+1)) + (getN(x+1,y+1)) + (getN(x-1,y)) + (getN(x+1,y)) + (getN(x-1,y-1)) + (getN(x,y-1)) + (getN(x+1,y-1))
+
+let squared(x: int): int = x * x
+
+let calcIndex(idx: int, len: int): int =
+  int (System.Math.Round((((float idx) / (float len)) % 1.0) * (float len)))
+
+let removeFirstIfFirstRound list =  list |> List.tail
+
 let Spiral n =
   let square = Array2D.create n n 0
   let nCur = ref (n*n)
@@ -17,10 +33,28 @@ let Spiral n =
   rangeC |> Seq.iteri (fun idx x -> square.[x,x] <- (rangeCval.[idx]) * (rangeCval.[idx]))
   square
 
-let nth = 603
-let spiral = Spiral nth
+let SpiralB(n: int) =
+  let square = Array2D.create n n 0
+  let nCur = ref 0
+  let nextNumber() = nCur := (!nCur+1) ; !nCur
+  let rangeC = [n/2..n-1]
+  square.[n/2, n/2] <- 1
+  let Frame num =
+    let rangeF = [num..(n - num - 2)]
+    let rangeB = [(n - num - 1)..(-1)..(num + 1)]
+    rangeB |> removeFirstIfFirstRound
+            |> Seq.iter (fun i -> square.[i,n-num-1] <- calcNeighbours(i,n-num-1, square))  //right
+    rangeB |> Seq.iter (fun i -> square.[num,i] <- calcNeighbours(num,i, square))          //top
+    rangeF |> Seq.iter (fun i -> square.[i,num] <- calcNeighbours(i,num, square))          //left
+    rangeF |> Seq.iter (fun i -> square.[n-num-1,i] <- calcNeighbours(n-num-1,i, square))  //bottom
+    let idx = calcIndex(Math.Abs(num-n/2), n/2)
+    let i = rangeC.[idx]
+    square.[i, i] <- calcNeighbours(i, i, square)        // corner
 
-let range = [(nth-1)..(-1)..0]
+  [(n/2 - 1)..(-1)..0] |> Seq.iter Frame
+  square
+
+let spiral = Spiral nth
 
 for x in range do
   for y in range do
@@ -28,4 +62,7 @@ for x in range do
 
 [<EntryPoint>]
 let main argv =
+  printfn "%A" (SpiralB 9 )
   0 // return an integer exit code
+
+
