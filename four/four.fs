3c29d0cb2be6889cf65d552f5969a5ee3539167a
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-05 21:16:46 +0200

    4A done

---
+++ four/four.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let inputFileToList(fileName: string): List<string[]> =
+  System.IO.File.ReadAllLines(fileName)
+    |> Seq.toList
+    |> List.map (fun x -> x.Split [|' '|])
+
+let containsDuplicate(input: seq<string*int>): bool =
+  input
+    |> Seq.toList
+    |> List.map (fun x -> (snd x))
+    |> List.forall ((=) 1)
+
+let uniques =
+  inputFileToList "./input.txt"
+    |> List.map (Seq.countBy id)
+    |> List.filter (fun x -> (containsDuplicate(x)))
+    |> List.length
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" uniques
+    0
