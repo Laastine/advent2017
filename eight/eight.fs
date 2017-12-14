c023540216d2ab670b4a656e02580d7d769aa8c6
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-14 16:05:33 +0200

    8A done

--- eight/eight.fs
+++ eight/eight.fs
@@ -1,9 +1,8 @@
 ﻿open System
 
-
 type Element = string*string*string*string*string*string
 
-let parseLine(input: string) =
+let parseLine(input: string): Element =
   let arr = input.Split([|"if "|], StringSplitOptions.None)
   let firstParts = arr.[0].Split([|' '|])
   let lastPart = arr.[1].Split([|' '|])
@@ -26,45 +25,65 @@ let evaluateOperation(variable: int, value: int, operation: string): int =
   elif operation = "dec" then variable - value
   else failwith "Didn't recognize operation"
 
+
 let getVariable(el: Element): string =
   let (variable, _, _, _, _, _) = el
   variable
 
-let isVariableInList(variable: string, list: List<Element>): bool =
-  list |> List.exists (fun x ->
-                                let listVariable = getVariable(x)
-                                variable = listVariable)
+let explode (s: string): List<char> = [for c in s -> c]
 
-let insertVariableToList(inputList: List<Element>): List<List<Element>> =
-  let rec recur(list: List<Element>, acc) =
+let removeMinusLetter(input: string): string =
+  let firstLetter = explode(input) |> List.head |> String.Concat
+  if firstLetter = "-" then explode(input) |> List.tail |> Array.ofList |> String.Concat
+  else input
+
+// For some reason "TryParse" won't work with negative numbers
+let isNumber(el: string): bool =
+  fst (System.Int32.TryParse(removeMinusLetter(el)))
+
+let getValue(acc: List<string*int>, el: string): int =
+  if isNumber(el) then (int el)
+  else
+    let someFound = acc |> List.tryFind (fun x -> el = (fst x))
+    if someFound.IsSome then (snd someFound.Value) else 0
+
+
+let setRegisterValue(acc: List<string*int>, el: string, value: int): List<string*int> =
+  let someFound = acc |> List.tryFind (fun x -> el = (fst x))
+  if someFound.IsSome then
+    List.map (fun x -> if el = (fst x) then (el, value) else x) acc
+  else (el, value)::acc
+
+let isVariableInList(variable: string, list: List<string*int>): bool =
+  List.exists (fun x -> variable = (fst x)) list
+
+let insertVariableToList(inputList: List<Element>): List<string*int> =
+  let rec recur(list: List<Element>, acc: List<string*int>) =
     match list with
     | [] -> acc
     | head::tail ->
-                    let headVariable = getVariable(head)
-                    let newAcc =
-                            if acc |> List.exists (fun e -> isVariableInList(headVariable, e)) then
-                                acc |> List.map (fun x ->
-                                                    if x.IsEmpty then x
-                                                    else
-                                                      if getVariable(x.Head) = headVariable then head::x
-                                                      else x)
-                            else [head]::acc
-                    recur(tail, newAcc)
+          let (headVariable, operation, value, varA, sign, varB) = head
+          let res = evaluateExpression(getValue(acc, varA), getValue(acc, varB), sign)
+          let newAcc =
+                  if res then
+                    let newRegVal = evaluateOperation(getValue(acc, headVariable), (int value), operation)
+                    setRegisterValue(acc, headVariable, newRegVal)
+                  else acc
+          recur(tail, newAcc)
   recur(inputList, [])
 
 let inputFileToList(fileName: string) =
   System.IO.File.ReadLines(fileName)
     |> Seq.toList
     |> List.map parseLine
+
+
+let calcA(inputList: List<Element>) =
+  inputList
     |> insertVariableToList
+    |> List.maxBy snd
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" ((inputFileToList "./eight2.txt"))
+    printfn "%A" (calcA(inputFileToList "./eight.txt"))
     0
-
-
-// b inc 5 if a > 1
-// a inc 1 if b < 5
-// c dec -10 if a >= 1
-// c inc -20 if c == 10
