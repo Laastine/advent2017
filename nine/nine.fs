2378d31a72f60b09a2a12df4e393bf9a8376c247
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-15 20:35:02 +0200

    Create project nine and parse input data

---
+++ nine/nine.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let explode (s: string): List<char> = [for c in s -> c]
+
+let inputFileToList(fileName: string) =
+  System.IO.File.ReadAllText(fileName)
+    |> explode
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (inputFileToList("./nine.txt"))
+    0 // return an integer exit code
