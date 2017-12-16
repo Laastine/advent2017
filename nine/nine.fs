8f2c9e82bee282a0f62c32e209fcf3e4fc00c09f
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-16 20:36:44 +0200

    9B done

--- nine/nine.fs
+++ nine/nine.fs
@@ -6,22 +6,21 @@ let inputFileToList(fileName: string) =
   System.IO.File.ReadAllText(fileName)
     |> explode
 
-let calcA(inputList: List<char>): int =
-  let rec recur(input: List<char>, isGarbage: bool, depth: int, acc: int): int =
+let calc(inputList: List<char>): int*int =
+  let rec recur(input: List<char>, isGarbage: bool, garbage: int, depth: int, acc: int): int*int =
     match input with
-    | [] -> acc
+    | [] -> garbage,acc
     | head::tail ->
-                    // printfn "acc %A" acc
-                    if head = '!' then recur(tail.Tail, isGarbage, depth, acc)
-                    elif not isGarbage && head = '{' then recur(tail, isGarbage, depth+1, acc+depth+1)
-                    elif not isGarbage && head = '}' then recur(tail, isGarbage, depth-1, acc)
-                    elif isGarbage && head <> '>' then recur(tail, isGarbage, depth, acc)
-                    elif not isGarbage &&  head = '<' then recur(tail, true, depth, acc)
-                    elif isGarbage && head = '>' then recur(tail, false, depth, acc)
-                    else recur(tail, isGarbage, depth, acc)
-  recur(inputList, false, 0, 0)
+                    if head = '!' then recur(tail.Tail, isGarbage, garbage, depth, acc)
+                    elif not isGarbage && head = '{' then recur(tail, isGarbage, garbage, depth+1, acc+depth+1)
+                    elif not isGarbage && head = '}' then recur(tail, isGarbage, garbage, depth-1, acc)
+                    elif isGarbage && head <> '>' then recur(tail, isGarbage, garbage+1, depth, acc)
+                    elif not isGarbage &&  head = '<' then recur(tail, true, garbage, depth, acc)
+                    elif isGarbage && head = '>' then recur(tail, false, garbage, depth, acc)
+                    else recur(tail, isGarbage, garbage, depth, acc)
+  recur(inputList, false, 0, 0, 0)
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" (inputFileToList("./nine.txt") |> calcA)
+    printfn "%A" (inputFileToList("./nine.txt") |> calc)
     0
