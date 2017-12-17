23c08bad0ce6c20c5ef65e7030710bc0a1e09fa4
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-17 19:33:03 +0200

    Create project ten and parse input data

---
+++ ten/ten.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let calcIndex(idx: int, len: int): int =
+  int (System.Math.Round((((float idx) / (float len)) % 1.0) * (float len)))
+
+let inputFileToList(fileName: string): List<int> =
+  System.IO.File.ReadAllText(fileName).Split([|','|])
+    |> Array.toList
+    |> List.map int
+
+[<EntryPoint>]
+let main argv =
+  printfn "%A" (inputFileToList "./ten2.txt")
+  0
