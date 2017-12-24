95d33cd5ee1ea1fde8334e05953d59286b7dc2e4
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-24 19:51:31 +0200

    Parse 12th input data

---
+++ twelve/twelve.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let parseLine(input: string): int*int[] =
+  let array = input.Trim().Split([|"<->"|], StringSplitOptions.None)
+  let values = array.[1].Split([|", "|], StringSplitOptions.None) |> Array.map int
+  ((int array.[0]), values)
+
+let inputFileToList(fileName: string) =
+  System.IO.File.ReadLines(fileName)
+    |> Seq.toList
+    |> List.map parseLine
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (inputFileToList "./twelve2.txt")
+    0
