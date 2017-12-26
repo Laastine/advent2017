52c261add89504c527622558522b32b569d70ac2
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-26 17:47:55 +0200

    12B done

--- twelve/twelve.fs
+++ twelve/twelve.fs
@@ -15,8 +15,10 @@ let inputFileToList(fileName: string) =
     |> Seq.toList
     |> List.map parseLine
 
-let findRoutes(inputList: List<Leaf>): int[] =
-  let zero: int[] = inputList |> List.filter (fun e -> e.node = 0) |> List.map (fun e -> e.leafs) |> Array.concat
+let findRoutes(inputList: List<Leaf>, nodeToFind: int): int[] =
+  let zero: int[] = inputList |> List.filter (fun e -> e.node = nodeToFind)
+                              |> List.map (fun e -> e.leafs)
+                              |> Array.concat
   let rec recur(input: List<Leaf>, acc: int[]): int[] =
     let newAcc = input
                     |> List.filter (fun e -> Array.exists ((=) (e.node)) acc)
@@ -29,10 +31,24 @@ let findRoutes(inputList: List<Leaf>): int[] =
     else recur(newList, newAcc)
   recur(inputList, zero)
 
+let findGroups(inputList: List<Leaf>): int[] =
+  let rec recur(nodeToInspect: int, subArray: int[], acc: int[]): int[] =
+    let newInput = inputList |> List.filter (fun e -> not (Array.exists ((=) e.node) subArray))
+    if newInput.IsEmpty then acc
+    else
+      let newNode = newInput.Head
+      let newNodes = Array.append acc [|newNode.node|]
+      let newSubArray = findRoutes(inputList, newNode.node) |> Array.append subArray |> Array.distinct
+      recur(newNode.node, newSubArray, newNodes)
+  recur(0, [||], [||])
+
 let calcA(input: int[]): int =
   input |> Array.length
 
+let calcB(input: List<Leaf>) = findGroups(input) |> Array.length
+
 [<EntryPoint>]
 let main argv =
-    printfn "%A" (inputFileToList "./twelve.txt" |> findRoutes |> calcA)
+    printfn "%A" (inputFileToList "./twelve.txt" |> (fun e -> findRoutes(e, 0)) |> calcA)
+    printfn "%A" (inputFileToList "./twelve.txt" |> calcB)
     0
