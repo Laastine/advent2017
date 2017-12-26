f83dac16a2dc2f84df990a1fb9d00a4b5d703c62
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-26 18:25:49 +0200

    Parse 13th input data

---
+++ thirteen/thirteen.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+type Layer = {
+  depth: int;
+  range: int
+}
+
+let parseLine(input: string): Layer =
+  let array = input.Trim().Split([|':'|])
+  printfn "%A" array
+  {depth = (int array.[0]); range = (int array.[1])}
+
+let inputFileToList(fileName: string): Layer[] =
+  System.IO.File.ReadLines(fileName)
+    |> Seq.toArray
+    |> Array.map parseLine
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (inputFileToList "./thirteen2.txt")
+    0
