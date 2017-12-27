47962c47b6a52aabdb4f8221feed3294cce547b5
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-27 20:08:32 +0200

    13A done

--- thirteen/thirteen.fs
+++ thirteen/thirteen.fs
@@ -7,7 +7,6 @@ type Layer = {
 
 let parseLine(input: string): Layer =
   let array = input.Trim().Split([|':'|])
-  printfn "%A" array
   {depth = (int array.[0]); range = (int array.[1])}
 
 let inputFileToList(fileName: string): Layer[] =
@@ -15,8 +14,28 @@ let inputFileToList(fileName: string): Layer[] =
     |> Seq.toArray
     |> Array.map parseLine
 
+let isMovingUp(tick: int, range: int): bool =
+  (tick/range) % 2 <> 0
+
+let calcScannerPos(tick: int, r: int): int*int =
+  let range = r-1
+  let index = tick % range
+  let isMovingUp = isMovingUp(tick, range)
+  if isMovingUp then
+    (Math.Abs(range-index),range)
+  else (index, range)
+
+let calcA(inputArray: Layer[]): int =
+  inputArray
+  |> Array.map (fun e ->
+                  let pos = fst (calcScannerPos(e.depth, e.range))
+                  let fatality = e.depth * e.range
+                  if pos = 0 then
+                      fatality
+                  else 0)
+  |> Array.sum
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" (inputFileToList "./thirteen2.txt")
-    0
+  (inputFileToList "./thirteen.txt" |> calcA |> printfn "%A")
+  0
