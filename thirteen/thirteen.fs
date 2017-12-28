98390028e73699b49c8e19ad0c6583b2ad322e44
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2017-12-28 19:05:54 +0200

    13B done

--- thirteen/thirteen.fs
+++ thirteen/thirteen.fs
@@ -17,25 +17,34 @@ let inputFileToList(fileName: string): Layer[] =
 let isMovingUp(tick: int, range: int): bool =
   (tick/range) % 2 <> 0
 
-let calcScannerPos(tick: int, r: int): int*int =
+let calcScannerPos(tick: int, r: int): int =
   let range = r-1
   let index = tick % range
   let isMovingUp = isMovingUp(tick, range)
-  if isMovingUp then
-    (Math.Abs(range-index),range)
-  else (index, range)
+  if isMovingUp then Math.Abs(range-index)
+  else index
+
+let isSafe tick (depth: int, scannerPos: int): bool =
+  (tick + depth) % scannerPos <> 0
 
 let calcA(inputArray: Layer[]): int =
   inputArray
   |> Array.map (fun e ->
-                  let pos = fst (calcScannerPos(e.depth, e.range))
-                  let fatality = e.depth * e.range
-                  if pos = 0 then
-                      fatality
+                  let pos = calcScannerPos(e.depth, e.range)
+                  if pos = 0 then e.depth * e.range
                   else 0)
   |> Array.sum
+
+let calcB(inputArray: Layer[]): int =
+  let detectors = Array.map (fun e -> (e.depth, 2 * (e.range - 1))) inputArray
+
+  let rec recur(x: int) =
+    let ticks = Array.forall (isSafe x) detectors
+    if ticks then x else recur(x+1)
+  recur(0)
 
 [<EntryPoint>]
 let main argv =
   (inputFileToList "./thirteen.txt" |> calcA |> printfn "%A")
+  (inputFileToList "./thirteen.txt" |> calcB |> printfn "%A")
   0
